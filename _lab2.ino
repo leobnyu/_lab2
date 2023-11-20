1fae457207159db1c43df2f418edfe1254ebdc67
leobnyu <151579854+leobnyu@users.noreply.github.com>
2023-11-20 18:17:15 -0500

    Initial commit

---
+++ _lab2.ino
@@ -0,0 +1,63 @@
+
+int potValue = 0;
+int randomGuy = 0;
+int ledPinArray[4] = { 32, 31, 30, 29 };
+int buttonPin = 33;
+int buttonPin2 = 34;
+int toggle = 35;
+int buttonPin3 = 36;
+int buttonPin4 = 39;
+
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPinArray[i], OUTPUT);
+  }
+  pinMode(buttonPin, INPUT);
+  pinMode(toggle, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
+}
+
+void loop() {
+  potValue = analogRead(A13);
+  delay(100);
+  if (digitalRead(buttonPin) == HIGH) {
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(ledPinArray[i], HIGH);
+      
+    }
+    delay(1000);
+    
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(ledPinArray[i], LOW);
+      
+    }
+    delay(1000);
+
+  } else if (digitalRead(buttonPin2) == HIGH and digitalRead(toggle) == HIGH) {
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(ledPinArray[i], HIGH);
+      delay(potValue);
+      digitalWrite(ledPinArray[i], LOW);
+      delay(potValue);
+    }
+
+  } else if (digitalRead(buttonPin2) == HIGH and digitalRead(toggle) == LOW) {
+    for (int i = 3; i > -1; i--) {
+      digitalWrite(ledPinArray[i], HIGH);
+      delay(potValue);
+      digitalWrite(ledPinArray[i], LOW);
+      delay(potValue);
+    }
+  }
+
+else if (digitalRead(buttonPin4) == HIGH) {
+  randomGuy = random(29, 33);
+  digitalWrite(randomGuy, HIGH);
+  delay(potValue);
+  digitalWrite(randomGuy, LOW);
+  delay(potValue);
+}
+}
